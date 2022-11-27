10b45e6dbf8a8c313858989c1bdb7143699d568d
Dejdesboisjolis <jeremyb.lebon@gmail.com>
2022-11-27 18:55:13 +0100

    first commit on bpps 828

--- src/pyramide.c
+++ src/pyramide.c
@@ -508,3 +508,56 @@ void passed_value (void)
     //printf("la valuer est : %d", **valeur);
 
 }
+
+void modification(void)
+{
+    printf("do nothing\n");
+}
+
+void die(char *s)
+{
+  perror(s);
+  exit(1);
+}
+
+struct msgbuf
+{
+    long    mtype;
+    char    mtext[MAXSIZE];
+};
+
+void message_queue(void)
+{
+    int msqid;
+    int msgflg = IPC_CREAT | 0666;
+    key_t key;
+    struct msgbuf sbuf;
+    size_t buflen;
+
+    key = 1234;
+
+    if ((msqid = msgget(key, msgflg )) < 0)   //Get the message queue ID for the given key
+      die("msgget");
+
+    //Message Type
+    sbuf.mtype = 1;
+
+    printf("Enter a message to add to message queue : ");
+    scanf("%[^\n]",sbuf.mtext);
+    getchar();
+
+    buflen = strlen(sbuf.mtext) + 1 ;
+
+    if (msgsnd(msqid, &sbuf, buflen, IPC_NOWAIT) < 0)
+    {
+        printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buflen);
+        die("msgsnd");
+        printf("commit bbps 828 numero 1 \n");
+        printf("commit bbps 828 numero 2 \n");
+    }
+
+    else
+        printf("Message Sent\n");
+
+    exit(0);
+}
