49bcd5235595b066c735ccf486ed3335269c9ff6
Dejdesboisjolis <jeremyb.lebon@gmail.com>
2022-11-27 17:40:25 +0100

    voici une erreur de rajoutée

--- src/pyramide.c
+++ src/pyramide.c
@@ -513,3 +513,37 @@ void modification(void)
 {
     printf("do nothing\n");
 }
+
+void message_queue(void)
+{
+    int msqid;
+    int msgflg = IPC_CREAT | 0666;
+    key_t key;
+    struct msgbuf sbuf;
+    size_t buflen;
+
+    key = 1234;
+
+    if ((msqid = msgget(key, msgflg )) < 0)   //Get the message queue ID for the given key
+      die("msgget");
+
+    //Message Type
+    sbuf.mtype = 1;
+
+    printf("Enter a message to add to message queue : ");
+    scanf("%[^\n]",sbuf.mtext);
+    getchar();
+
+    buflen = strlen(sbuf.mtext) + 1 ;
+
+    if (msgsnd(msqid, &sbuf, buflen, IPC_NOWAIT) < 0)
+    {
+        printf ("%d, %d, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buflen);
+        die("msgsnd");
+    }
+
+    else
+        printf("Message Sent\n");
+
+    exit(0);
+}
